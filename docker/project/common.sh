79c1dfaefc0130e0d69cdbf2e6ac4016538ca002
ArthurWuTW <lion811004@gmail.com>
2021-08-07 13:08:50 +0800

    project-postgresql-db

--- docker/project/common.sh
+++ docker/project/common.sh
@@ -1,13 +1,13 @@
 REPO_DIR=$(realpath $(realpath $(dirname $PWD)/dependencies))
 HOSTNAME=$(cat /etc/hostname)
 DOCKER_DIR=$REPO_DIR
-IMAGE_NAME=${IMAGE_NAME:-project}
+IMAGE_NAME=${IMAGE_NAME:-project-postgresql-db}
 CONTAINER_USER=user
 CONTAINER_HOME=/home/$CONTAINER_USER
 CONTAINER_REPO_DIR=/repository
 CONTAINER_PERSISTENT_DIR=/persistent
 CONTAINER_PERSISTENT_VOLUME=project-persistent-data
-CONTAINER_NAME=project
+CONTAINER_NAME=project-postgresql-db
 
 # NVIDIA_DRIVER_VERSION=$(nvidia-smi -q | grep "Driver Version" | tr -s ' ' | cut -d ' ' -f4)
 # NVIDIA_DRIVER_VERSION_MAJOR=$(echo $NVIDIA_DRIVER_VERSION | cut -d '.' -f1)
